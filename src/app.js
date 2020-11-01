470a9e3efd51f4b3760f95d23e8d89260beca32e
rhernandez2601 <rhernandez2601@outlook.com>
2020-11-01 16:06:12 -0500

    update

--- src/app.js
+++ src/app.js
@@ -10,9 +10,13 @@ router.get("/", (req, response) => {
 
     const request = require('request');
 
-    const client_id = process.env.CLIENT_ID;
-    const client_secret = process.env.CLIENT_SECRET;
-    const refresh_token = process.env.REFRESH_TOKEN;
+    const client_id = "37eaba6e04e343259fc399757e7e2e75";
+    const client_secret = "80ef3f8e3de94432933dc6ce1cfd7e63";
+    const refresh_token = "AQAEWNFSUmE-vVgfhbbhthyRvyzjzv13nVjr5qdvx_BboGh4axBUHGo0CRYFS1eV0vCBs_n_gwCh7_K7t45gnLkp-Msie7ZdigbF6hONz0-J3Z642H8RFfFbA72QL1g7D_0";
+
+    // const client_id = process.env.CLIENT_ID;
+    // const client_secret = process.env.CLIENT_SECRET;
+    // const refresh_token = process.env.REFRESH_TOKEN;
 
     var authOptions = {
         url: 'https://accounts.spotify.com/api/token',
@@ -27,6 +31,7 @@ router.get("/", (req, response) => {
             if((res.statusCode == 204) || JSON.parse(body).currently_playing_type != "track") {
                 request({ url:"https://api.spotify.com/v1/me/player/recently-played?type=track&limit=1", headers: header }, function(error, res, body) {
                     var body_text = JSON.parse(body);
+                    console.log(body_text);
                     var track = body_text.items[0].track;
                     song_name = track.name;
                     artist = track.artists[0].name;
